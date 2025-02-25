f8d87673b0bd1b9ea2892f2257554ddd2fc8d89c
Mulet-J <71126090+Mulet-J@users.noreply.github.com>
2025-02-25 16:11:44 +0100

    init

---
+++ DesktopEye/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DesktopEye.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ DesktopEye/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DesktopEye.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
